abde3689581a7c2c311337d6849888a10c604749
F0RG-2142 <vanaardtlj03@gmail.com>
2026-07-19 20:37:29 +0200

    used new endpoint cause I cant read docs apparently

--- apiCalls/apiCalls.go
+++ apiCalls/apiCalls.go
@@ -14,52 +14,62 @@ type currencyID struct {
 	ItemId         int `json:"ItemID"`
 }
 
-type currencyPair struct {
-	Data []currencyPairData `json:"Data"`
+type PriceLogs struct {
+	Price    float32 `json:"Price"`
+	Time     string  `json:"Time"`
+	Quantity int     `json:"Quantity"`
 }
 
-type currencyPairData struct {
-	Epoch     int     `json:"Epoch"`
-	MarketCap float32 `json:"MarketCap"`
-	Volume    int     `json:"Volume"`
+type currencyItems struct {
+	UniqueItemId    int         `json:"UniqueItemId"`
+	ItemId          int         `json:"ItemId"`
+	Name            string      `json:"Name"`
+	Type            string      `json:"Type"`
+	PriceLogs       []PriceLogs `json:"PriceLogs"`
+	CurrentPrice    float32     `json:"CurrentPrice"`
+	CurrentQuantity int32       `json:"CurrentQuantity"`
 }
 
-func CallApi(client *http.Client, leagueName, currencyName1, currencyName2 string, limit int32) ([]currencyPairData, error) {
-	item1Id, err := findItemId(client, leagueName, currencyName1)
-	if err != nil {
-		fmt.Printf("Error finding item %s: %v\n", currencyName1, err)
-		return []currencyPairData{}, nil
-	}
-	item2Id, err := findItemId(client, leagueName, currencyName2)
-	if err != nil {
-		fmt.Printf("Error finding item %s: %v\n", currencyName2, err)
-		return []currencyPairData{}, nil
-	}
+type currencyData struct {
+	Total int             `json:"Total"`
+	Pages float32         `json:"Pages"`
+	Items []currencyItems `json:"Items"`
+}
 
-	reqUrl := fmt.Sprintf("%s/%s/Currencies/Pairs/%d/%d/History", baseUrl, leagueName, item1Id, item2Id)
+func CallApi(client *http.Client, leagueName string, pageNumber int32) ([]currencyItems, error) {
+
+	reqUrl := fmt.Sprintf("%s/%s/Currencies/ByCategory?category=currency&pageNumber=%d&pageSize=25", baseUrl, leagueName, pageNumber)
 	fmt.Println(reqUrl)
 	resp, err := client.Get(reqUrl)
 	if err != nil {
-		return []currencyPairData{}, fmt.Errorf("error executing GET request: %w", err)
+		return []currencyItems{}, fmt.Errorf("error executing GET request: %w", err)
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		return []currencyPairData{}, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
-	}
-
 	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return []currencyPairData{}, fmt.Errorf("error reading the response body: %w", err)
+		return []currencyItems{}, fmt.Errorf("error reading the response body: %w", err)
 	}
 
-	var respStruct currencyPair
+	if resp.StatusCode != http.StatusOK {
+		return []currencyItems{}, fmt.Errorf("unexpected status code %d: %s", resp.StatusCode, string(bodyBytes))
+	}
+
+	var respStruct currencyData
 
 	if json.Unmarshal(bodyBytes, &respStruct) != nil {
-		return []currencyPairData{}, fmt.Errorf("error unmarshaling response: %w", err)
+		return []currencyItems{}, fmt.Errorf("error unmarshaling response: %w", err)
 	}
 
-	return respStruct.Data, nil
+	if len(respStruct.Items) > 0 {
+		fmt.Println(respStruct.Items[0])
+	} else {
+		fmt.Println("The request succeeded, but the items array is empty.")
+	}
+
+	fmt.Println(respStruct.Items[0])
+
+	return respStruct.Items, nil
 }
 
 func findItemId(client *http.Client, leagueName, currencyName string) (int, error) {
--- main.go
+++ main.go
@@ -9,14 +9,12 @@ import (
 // TokenResponse matches the JSON structure returned by the PoE OAuth server
 func main() {
 	client := http.DefaultClient
-	data, err := apiCalls.CallApi(client, "runes", "chaos", "exalted", 100)
+	data, err := apiCalls.CallApi(client, "runes", 1)
 	if err != nil {
 		fmt.Printf("error calling data: %s", err)
 		return
 	}
 	for _, v := range data {
-		fmt.Printf("%d", v.Epoch)
-		fmt.Printf("%f", v.MarketCap)
-		fmt.Printf("%d", v.Volume)
+		fmt.Printf("%d", v.PriceLogs[0].Quantity)
 	}
 }
